2f11465a8b65fc5e101a5327c3fdc35e930f0b50
lons <lonsnw@gmail.com>
2024-04-09 11:39:53 -0500

    Set up form

--- src/components/GalleryAdd/GalleryAdd.jsx
+++ src/components/GalleryAdd/GalleryAdd.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-
+import Button from '@mui/material/Button';
 
 function GalleryAdd() {
     const [url, setUrl] = useState('');
@@ -15,13 +15,59 @@ function GalleryAdd() {
             setTitle('');
             setDescription('');
             getGallery()
+        }).catch((error) => {
+            console.error(error);
+            alert('Error adding your image');
         })
-
     }
+
+    const submitForm = (e) => {
+        e.preventDefault();
+        addImage();
+    }
+
     return (
-        <>
-        
-        </>
+        <div className="add-div">
+        <h2>Add to list</h2>
+        <form onSubmit={submitForm}>
+        <label className="label">
+            Item:
+        </label>
+        <input
+            className="input"
+            type="text"
+            placeholder="Item"
+            value={url}
+            onChange={(evt) => setUrl(evt.target.value)}
+        />
+        <br />
+        <label className="label">
+            Image URL:
+        </label>
+        <input
+            className="input"
+            type="text"
+            placeholder="title"
+            value={title}
+            onChange={(evt) => setTitle(evt.target.value)}
+        />
+        <br />
+        <label className="label">
+            Unit:
+        </label>
+        <input
+            className="input"
+            type="text"
+            placeholder="description"
+            value={description}
+            onChange={(evt) => setDescription(evt.target.value)}
+        />
+        <br />
+        <div>
+            <Button variant="contained" color="primary" type="submit" className="add-button">Add to list</Button>
+        </div>
+        </form>
+    </div>
     )
 }
 
