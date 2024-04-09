360565834e367cbcd4214346d9c5930d397af1a3
lons <lonsnw@gmail.com>
2024-04-09 11:05:23 -0500

    Adding DELETE on the client side and DOM

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -7,11 +7,15 @@ function App() {
     return (
       <div>
         <header>
-          <h1>React Gallery</h1>
+          <h1>Photo Gallery</h1>
         </header>
         <main>
           <GalleryList images={images} setImages={setImages} />
         </main>
+        <footer>
+          <p style={{fontSize:12}}>Please note: I did have the gallery image using an img tag before implementing MUI; see commit f565b7c.</p>
+          <p style={{fontSize:10}}>I didn't read ahead enough to see that MUI was a stretch goal and should have been branched.  I know we're supposed to read the whole thing first... perhaps for this reason.  I will start including reading the stretch goals in that.</p>
+        </footer>
       </div>
     );
 }
