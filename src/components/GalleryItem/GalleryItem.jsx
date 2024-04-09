c5756a000645da08285e40f13adcfd2ae6d31788
lons <lonsnw@gmail.com>
2024-04-09 09:07:32 -0500

    Correcting flip card

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -2,18 +2,20 @@ import React, { useState, useEffect } from 'react';
 
 function GalleryItem ({image}) {
     // resource: https://medium.com/@amie.n.foster/flipping-out-how-to-create-a-card-that-flips-with-react-and-css-131dba54fc96
-    const[flip, setFlip] = useState(false);
+    const[flip, setFlip] = useState(true);
 
     return (
         <div data-testid="galleryItem" key={image.id} >
-            <div data-testid="toggle" className={`card ${flip ? "flip" : ""}`}>
-                <div className="front" onClick={() => setFlip(!flip)}>
+            <div data-testid="toggle" onClick={() => setFlip(!flip)}>
+                {flip ? (
+                <div className="front" >
                     <img src={image.url} className='front'></img>
                 </div>
+                ) : (
                 <div className="back" onClick={() => setFlip(!flip)}>
                     <p className='back'>{image.description}</p>
-
                 </div>
+                )}
             </div>
             <h5>{image.likes}</h5>
         </div>
