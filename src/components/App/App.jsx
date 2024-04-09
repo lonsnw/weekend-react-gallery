5197363cd2779a6c5e95eb0dc17e00a44491de65
lons <lonsnw@gmail.com>
2024-04-09 08:17:53 -0500

    Moving GET to GalleryList component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,27 +5,13 @@ import GalleryList from "../GalleryList/GalleryList.jsx";
 function App() {
   let [images, setImages] = useState([]);
 
-  useEffect(() => {
-    getGallery()
-  }, []);
-
-  const getGallery = () => {
-    axios.get('/api/gallery').then((response) => {
-      console.log('Data:', response.data);
-      setImages(response.data);
-    }).catch((error) => {
-      console.error(error);
-      alert('There was an error loading the gallery.');
-    });
-  };
-
     return (
       <div>
         <header>
           <h1>React Gallery</h1>
         </header>
         <main>
-          <GalleryList images={images} />
+          <GalleryList images={images} setImages={setImages} />
         </main>
       </div>
     );
--- src/components/GalleryList/GalleryList.jsx
+++ src/components/GalleryList/GalleryList.jsx
@@ -1,6 +1,23 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import GalleryItem from "../GalleryItem/GalleryItem.jsx";
 
-function GalleryList ({images}) {
+function GalleryList ({images, setImages}) {
+
+    useEffect(() => {
+        getGallery()
+      }, []);
+
+    const getGallery = () => {
+        axios.get('/api/gallery').then((response) => {
+          console.log('Data:', response.data);
+          setImages(response.data);
+        }).catch((error) => {
+          console.error(error);
+          alert('There was an error loading the gallery.');
+        });
+      };
+
     return (
         <div data-testid="galleryList">
         {images.map((image) => {
